2ce9825ed6c1fafc9d45c5d6c6767fe4d6b26f64
Johary Rakotomalala <johary.rakotomalala.31@gmail.com>
2023-03-23 19:00:56 +0100

    First initialization

---
+++ Camera/Gimbal.gd
@@ -0,0 +1,69 @@
+extends Node3D
+
+@onready var camera = $InnerGimbal/Camera3D
+@onready var innergimbal = $InnerGimbal
+
+@export var max_zoom = 3.0
+@export var min_zoom = 0.5
+@export var zoom_speed = 0.08
+var zoom = 1.5
+
+@export var speed = 0.3
+@export var drag_speed = 0.005
+@export var acceleration = 0.08
+@export var mouse_sensitivity = 0.005
+
+var move = Vector3()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _input(event):
+	if Input.is_action_pressed("rotate_cam"):
+		if event is InputEventMouseMotion:
+			if event.relative.x != 0:
+				rotate(Vector3.UP, -event.relative.x * mouse_sensitivity)
+	if Input.is_action_pressed("move_cam"):
+		if event is InputEventMouseMotion:
+			move.x -= event.relative.x * drag_speed
+			move.z -= event.relative.y * drag_speed
+	
+	if event.is_action_pressed("zoom_in"):
+		zoom -= zoom_speed
+	if event.is_action_pressed("zoom_out"):
+		zoom += zoom_speed
+	zoom = clamp(zoom, min_zoom, max_zoom)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#zoom camera
+	scale = lerp(scale, Vector3.ONE * zoom, zoom_speed)
+	#clamp rotation
+	innergimbal.rotation.x = clamp(innergimbal.rotation.x, -1.1, 0.3)
+	#move camera
+	move_cam(delta)
+
+func move_cam(delta):
+	#get inputs
+	if Input.is_action_pressed("move_forward"):
+		move.z = lerp(move.z,-speed, acceleration)
+	elif Input.is_action_pressed("move_backward"):
+		move.z = lerp(move.z,speed, acceleration)
+	else:
+		move.z = lerp(move.z, 0.0, acceleration)
+	if Input.is_action_pressed("move_left"):
+		move.x = lerp(move.x,-speed, acceleration)
+	elif Input.is_action_pressed("move_right"):
+		move.x = lerp(move.x,speed, acceleration)
+	else:
+		move.x = lerp(move.x, 0.0, acceleration)
+	
+	#move camera
+	position += move.rotated(Vector3.UP,self.rotation.y) * zoom
+	position.x = clamp(position.x,-20,20)
+	position.z = clamp(position.z,-20,20)
+
+
